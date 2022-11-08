14fd30e11338921fa71d16816fc0467334d9bfab
jwhong <jwhong98@gmail.com>
2022-11-07 16:41:53 -0800

    added images and displaying to page

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const imgArray = [
+  "img1.webp",
+  "img2.webp",
+  "img3.webp",
+  "img4.webp",
+  "img5.webp",
+];
 const container = document.querySelector("#container");
+
+const createImageCard = (img) => {
+  const card = document.createElement("div");
+  card.classList.add("imgCard");
+  const cardImg = new Image();
+  cardImg.src = "./assets/" + img;
+  card.appendChild(cardImg);
+  return card;
+};
+
+imgArray.map((img) => container.appendChild(createImageCard(img)));

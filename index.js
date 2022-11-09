8397c7fd08ea460a01a348907e4731c98574a0e4
jwhong <jwhong98@gmail.com>
2022-11-09 13:49:11 -0800

    added navigation dots and 5 second cycling for images

--- index.js
+++ index.js
@@ -2,25 +2,54 @@ const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
 const images = document.querySelectorAll(".imgCard");
+const navContainer = document.querySelector(".navDotContainer");
 
 let currentIndex = 0;
 
-nextBtn.addEventListener("click", () => {
-  if (currentIndex === 4) {
-    currentIndex = 0;
-  } else {
-    currentIndex += 1;
-  }
-  [...images].map((el) => el.classList.remove("active"));
-  images[currentIndex].classList.add("active");
-});
-
-prevBtn.addEventListener("click", () => {
+const prev = () => {
   if (currentIndex === 0) {
     currentIndex = 4;
   } else {
     currentIndex -= 1;
   }
+  setActiveImg(currentIndex);
+  setDotActive(currentIndex);
+};
+
+const next = () => {
+  if (currentIndex === 4) {
+    currentIndex = 0;
+  } else {
+    currentIndex += 1;
+  }
+  setActiveImg(currentIndex);
+  setDotActive(currentIndex);
+};
+
+setInterval(next, 5000);
+
+const createNavDot = (i) => {
+  const dot = document.createElement("div");
+  dot.classList.add("navDot");
+  dot.dataset.pos = i;
+  dot.addEventListener("click", () => {
+    setActiveImg(i);
+    setDotActive(i);
+  });
+  return dot;
+};
+
+[...images].map((el, i) => navContainer.appendChild(createNavDot(i)));
+
+const setActiveImg = (index) => {
   [...images].map((el) => el.classList.remove("active"));
-  images[currentIndex].classList.add("active");
-});
+  images[index].classList.add("active");
+};
+
+const dots = document.querySelectorAll(".navDot");
+dots[0].classList.add("activeDot");
+
+const setDotActive = (index) => {
+  dots.forEach((el) => el.classList.remove("activeDot"));
+  dots[index].classList.add("activeDot");
+};
